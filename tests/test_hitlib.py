332c4e4294304bebd7e9c7a3289849fd626fb6f8
Naphong Chadha <harrajc@gmail.com>
2023-01-27 17:17:01 +0700

    Fixed files

--- tests/test_hitlib.py
+++ tests/test_hitlib.py
@@ -4,14 +4,10 @@ from automated_clean_code.exercise_20_histlib import find_max_and_min_key, get_l
 def test_get_lines_from_file():
     output = {"Superman 1": 1, "Superman 2": 1, "Superman 3": 2, "": 1}
     output2 = {"a": 7, "": 3, "ap": 10}
-    assert output == get_lines_from_file(
-        "/Users/naphong/Desktop/Uni/softwareeng/automated-clean-code/tests/superman.txt"
-    )
-    assert output2 == get_lines_from_file("/Users/naphong/Desktop/Uni/softwareeng/automated-clean-code/tests/ap.txt")
+    assert output == get_lines_from_file("tests/superman.txt")
+    assert output2 == get_lines_from_file("tests/ap.txt")
 
 
 def test_find_max_and_min_key():
     output = ("", 3, "ap", 10)
-    assert output == find_max_and_min_key(
-        get_lines_from_file("/Users/naphong/Desktop/Uni/softwareeng/automated-clean" "-code/tests/ap.txt")
-    )
+    assert output == find_max_and_min_key(get_lines_from_file("tests/ap.txt"))
